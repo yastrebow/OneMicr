05aa2e232238f20ea38d2b81e8d00e65caf84ff8
yastrebow <yastrebow@gmail.com>
2021-10-25 14:03:18 +0300

    Initial commit

---
+++ src/main/java/ru/yastrebov/onemicr/OneMicrApplication.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.onemicr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMicrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMicrApplication.class, args);
+    }
+
+}
